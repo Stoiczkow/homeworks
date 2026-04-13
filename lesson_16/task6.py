984e8b3c68c978e4e2bbddb24293f7f8211b582b
Yakhel <daniel.chomiak90@gmail.com>
2026-04-13 15:25:52 +0200

    zadania 1-6

---
+++ lesson_16/task6.py
@@ -0,0 +1,30 @@
+# Zadanie 6. Klasa Request : Napisz klasę w Pythonie o nazwie HttpRequest.
+
+class HttpRequest:
+    def __init__(self, method, target, headers=None, body=None):
+        self.method = method
+        self.target = target
+        if headers is None:
+            self.headers = {}
+        else:
+            self.headers = headers
+        self.body = body
+    
+    def display(self):
+        print("---HTTP Request---")
+        print(f"Method: {self.method}")
+        print(f"Target: {self.target}")
+        print("Headers:")
+        for key, value in self.headers.items():
+            print(f" {key}: {value}")
+        print("Body: ")
+        if self.body is None:
+            print("(empty)")
+        else:
+            print(f"{self.body}")
+
+request = HttpRequest("POST", 
+                      "/index.html", 
+                      {"Host": "example.com", "User-Agent": "PythonClient/1.0"}, '{"title": "Nowy wpis", "content": "Treść"}' 
+                      )
+request.display()
