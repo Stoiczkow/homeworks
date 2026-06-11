950127879d6171d4d8e9df569036b8d52ac0806b
szmydt <szymon.szmydt@wm.wroc.pl>
2026-06-11 09:16:50 +0200

    feat: task_4 i task_5

---
+++ lesson_20/product/views.py
@@ -0,0 +1,7 @@
+from django.views.generic import ListView
+from product.models import Product
+
+class ProductView(ListView):
+    model = Product
+    template_name = 'product_list.html'
+    context_object_name = 'products'
